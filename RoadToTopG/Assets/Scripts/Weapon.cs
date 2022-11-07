b14d51251e9523e9325111428c077d2479f29508
JabArmen <armenjamgotchian7@gmail.com>
2022-11-07 00:23:52 -0500

    #54 second phase(part1) of gun mechanics
    DONE:
    -weapon switching added
    -muzzle flash changed to a better one
    -reload animation and ammunition
    -gun placement changed
    -pistol added
    TODO:
    -add equip, shoot and reload sound effects
    -add bullet tracers
    -add ammo UI
    -add more guns

--- RoadToTopG/Assets/Scripts/Weapon.cs
+++ RoadToTopG/Assets/Scripts/Weapon.cs
@@ -1,21 +1,51 @@
 
 using UnityEngine;
+using System.Collections;
 
 public class Weapon : MonoBehaviour
 {
 
+    //shooting variables
     public float damage = 10f;
     public float range = 100f;
-    public float fireRate = 2f;
+    public float fireRate = 1f;
+    private float nextTimeToFire = 0f;
 
+    //reload and ammunition variables
+    public int maxAmmo = 10;
+    private int currentAmmo;
+    public float reloadTime = 1f;
+    private bool isReloading = false;
+    public Animator animator;
+
+    //effects variables
     public Camera fpsCam;
     public ParticleSystem muzzleFlash;
     public GameObject impactEffect;
 
-    private float nextTimeToFire = 0f;
-    // Update is called once per frame
+
+    void Start () 
+    {
+        currentAmmo = maxAmmo;
+    }
+
+    void OnEnable()
+    {
+        isReloading = false;
+        animator.SetBool("Reloading",false);
+    }
+
     void Update()
     {
+        if (isReloading)
+            return;
+
+        if (currentAmmo <= 0)
+        {
+            StartCoroutine(Reload());
+            return;
+        }
+
         if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire){
             nextTimeToFire = Time.time + (1f/fireRate);
             Shoot();
@@ -24,7 +54,7 @@ public class Weapon : MonoBehaviour
 
     //will get called every time user shoots
     void Shoot(){
-        //muzzle flash particle effect
+        currentAmmo--;
         muzzleFlash.Play();
         RaycastHit hit;
         
@@ -40,7 +70,17 @@ public class Weapon : MonoBehaviour
         //destroys impact particle system instantiated
         GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
         Destroy(impactGO, 0.5f);
-
         }
+    }
+
+    IEnumerator Reload()
+    {
+        isReloading = true;
+        animator.SetBool("Reloading",true);
+        yield return new WaitForSeconds(reloadTime - .25f);
+        animator.SetBool("Reloading",false); 
+        yield return new WaitForSeconds(.25f);
+        currentAmmo = maxAmmo;
+        isReloading = false;
     }
 }
