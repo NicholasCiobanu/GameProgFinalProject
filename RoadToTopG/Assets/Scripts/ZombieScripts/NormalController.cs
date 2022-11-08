37fc4de5e500429ffcc3a49968d3e59aef59f550
NicholasCiobanu <ciobanunicholas@gmail.com>
2022-11-08 17:30:25 -0500

    completed animations and added proper hitboxes to all zombies

--- RoadToTopG/Assets/Scripts/ZombieScripts/NormalController.cs
+++ RoadToTopG/Assets/Scripts/ZombieScripts/NormalController.cs
@@ -4,16 +4,22 @@ using UnityEngine;
 
 public class NormalController : MonoBehaviour
 {
-    
+    [SerializeField]
+    private int health = 100;
+    Animator animator;
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();    
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (health == 0)
+        {
+            animator.SetBool("isDead", true);
+        }
     }
 }
---
+++ RoadToTopG/Assets/Scripts/ZombieScripts/RunState.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class RunState : StateMachineBehaviour
+{
+    private GameObject player;
+    private GameObject zombie;
+    
+    private float distance;
+    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
+    override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        player = GameObject.Find("PlayerCapsule");
+        zombie = animator.GameObject();
+        
+        
+    }
+
+    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
+    override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+        distance = Vector3.Distance(player.transform.position, zombie.transform.position);
+        
+        Debug.Log("Distance to player" + distance);
+        if(distance < 1)
+        {
+            animator.SetBool("isInRange", true);
+        }
+        
+        
+    }
+
+    // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
+    override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    {
+
+    }
+
+    // OnStateMove is called right after Animator.OnAnimatorMove()
+    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that processes and affects root motion
+    //}
+
+    // OnStateIK is called right after Animator.OnAnimatorIK()
+    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
+    //{
+    //    // Implement code that sets up animation IK (inverse kinematics)
+    //}
+}
