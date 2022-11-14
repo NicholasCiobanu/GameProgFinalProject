7115e9242f6483d295d93962191873b822ada14a
BANNIR <74209018+BANNIR@users.noreply.github.com>
2022-11-14 01:20:58 -0500

    Added Currency (#60)
    * Added currency
    
    Doors now require money to open
    
    What's left:
    Gain money when shooting zombies
    The Shop

---
+++ RoadToTopG/Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour
+{
+    
+    [SerializeField]
+    private Text moneyText;
+    private int currentMoney;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentMoney = 9999;
+        moneyText.text = "Money: " + currentMoney;
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void AddMoney(int moneyToAdd) {
+        currentMoney += moneyToAdd;
+        moneyText.text = "Money: " + currentMoney;
+    }
+
+    public void RemoveMoney(int moneyToRemove) {
+        currentMoney -= moneyToRemove;
+        moneyText.text = "Money: " + currentMoney;
+    }
+
+    public int getMoney()
+    {
+        return currentMoney;
+    }
+}
