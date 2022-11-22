f09ff183f60110eb45825abf12063354d8b298b4
Ali Alnajjar <Ali.nj.95@hotmail.com>
2022-11-22 00:52:35 -0500

    Fixed cutscene and women spawnings

--- RoadToTopG/Assets/Scripts/CutScenes.cs
+++ RoadToTopG/Assets/Scripts/CutScenes.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 // using UnityEngine.SceneManagement;
 
 public class CutScenes : MonoBehaviour
@@ -16,6 +17,8 @@ public class CutScenes : MonoBehaviour
     [SerializeField] private SneakoController sneakoController;
     [SerializeField] private Animator Kanye = null;
     [SerializeField] private KanyeController kanyeController;
+    [SerializeField] private GameObject crosshair;
+    [SerializeField] private GameObject crosshair1;
 
     [SerializeField] private Animator AndrewTate = null;
     [SerializeField] private AndrewTateController andrewController;
@@ -26,9 +29,11 @@ public class CutScenes : MonoBehaviour
     void Start()
     {
         TristanTate.GetComponent<Animator>();
-        Sneako.GetComponent<Animator>();
-        Kanye.GetComponent<Animator>();
-        AndrewTate.GetComponent<Animator>();
+        //Sneako.GetComponent<Animator>();
+        //Kanye.GetComponent<Animator>();
+        //AndrewTate.GetComponent<Animator>();
+        
+
 
         StartCoroutine(TheSequence());
     }
@@ -36,19 +41,23 @@ public class CutScenes : MonoBehaviour
     IEnumerator TheSequence()
     {
         yield return new WaitForSeconds(8);
+        //crosshair.gameObject.SetActive(false);
+        //crosshair1.gameObject.SetActive(false);
         Cam2.SetActive(true);
         Cam1.SetActive(false);
         yield return new WaitForSeconds(9);
         Cam3.SetActive(true);
         Cam2.SetActive(false);
         TristanTate.enabled = (true);
-        Sneako.enabled = (true);
-        Kanye.enabled = (true);
-        AndrewTate.enabled = (true);
+        //Sneako.enabled = (true);
+        //Kanye.enabled = (true);
+        //AndrewTate.enabled = (true);
 
-        andrewController.enabled = (true);
-        kanyeController.enabled = (true);
-        sneakoController.enabled = (true);
+        //andrewController.enabled = (true);
+        //kanyeController.enabled = (true);
+        //sneakoController.enabled = (true);
         tristanController.enabled = (true);
+        crosshair.SetActive(true);
+        crosshair1.SetActive(true);
     }
 }
