bfcd08a72c17009fdc0233a03fd5c1f65e953fcd
shahe <%email%>
2022-11-22 10:21:00 -0500

    buy gun half implemented

--- RoadToTopG/Assets/Scripts/AmmoShop.cs
+++ RoadToTopG/Assets/Scripts/AmmoShop.cs
@@ -26,7 +26,7 @@ public class AmmoShop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        weapon = FindObjectOfType<Weapon>();
     }
 
     private void OnTriggerStay(Collider other)
--- RoadToTopG/Assets/Scripts/BuyGun.cs
+++ RoadToTopG/Assets/Scripts/BuyGun.cs
@@ -4,15 +4,51 @@ using UnityEngine;
 
 public class BuyGun : MonoBehaviour
 {
+    private MoneyManager mm;
+    bool flag = false;
+    private Weapon currentWeapon;
+    [SerializeField]
+    private GameObject toBuy;
+    [SerializeField]
+    private int cost;
+    [SerializeField]
+    private GameObject uiObject;
+    [SerializeField]
+    private GameObject guns;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentWeapon = FindObjectOfType<Weapon>();
+        mm = FindObjectOfType<MoneyManager>();
+        uiObject.SetActive(false);
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        currentWeapon = FindObjectOfType<Weapon>();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.name == "PlayerCapsule")
+        {
+            if (!flag)
+            {
+                uiObject.SetActive(true);
+                flag = true;
+            }
+
+            if (Input.GetKeyDown(KeyCode.B) && mm.getMoney() >= cost)
+            {
+                mm.RemoveMoney(cost);
+                uiObject.SetActive(false);
+                currentWeapon.transform.DetachChildren();
+                toBuy.transform.parent = guns.transform;
+                Debug.Log("Purchased");
+            }
+        }
+
     }
 }
