f67bc72b3f589576625c0c642cafd7d3b916fca7
rol3293 <32209254+rol3293@users.noreply.github.com>
2022-11-23 23:35:49 -0500

    shooting karen removes health (#92)

--- RoadToTopG/Assets/Scripts/KarenController.cs
+++ RoadToTopG/Assets/Scripts/KarenController.cs
@@ -5,17 +5,20 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.AI;
-public class NormalController : MonoBehaviour
+public class KarenController : MonoBehaviour
 {
     [SerializeField]
     public int health;
     Animator animator;
 
+    [SerializeField] 
+    private UIInterfaceController UIController;
     
 
     [SerializeField]
     private NavMeshAgent NavMeshAgent;
 
+    [SerializeField]
     private Transform Player;
     
 
@@ -38,7 +41,6 @@ public class NormalController : MonoBehaviour
         
         if (health < 0)
         {
-        
             animator.SetBool("isDead", true);
         } else {
             NavMeshAgent.SetDestination(Player.position);
@@ -46,9 +48,11 @@ public class NormalController : MonoBehaviour
 
 
     }
-
-    
         
-    
+    public void ReduceHealth(int dmg) 
+    {
+        health -= dmg;
+        UIController.setHealth(UIController.getHealth() - dmg);
+    }
     
 }
