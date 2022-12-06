2064a0c130ebbf43e1b0bda0c9fa8c60dc170d9f
BANNIR <74209018+BANNIR@users.noreply.github.com>
2022-12-06 17:48:18 -0500

    scattered the shops, resized them and added another powerup (#105)
    Made a extra damage PowerUp and made a Text UI that tells the player what power up they got

--- RoadToTopG/Assets/Scripts/PowerUpShop.cs
+++ RoadToTopG/Assets/Scripts/PowerUpShop.cs
@@ -1,21 +1,27 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PowerUpShop : MonoBehaviour
 {
     [SerializeField]
     private GameObject uiObject;
+    [SerializeField]
+    private Text powerNotif;
     bool flag = false;
     [SerializeField]
     private int cost;
     private MoneyManager mm;
 
     private UIInterfaceController ic;
+    [SerializeField] Weapon m4;
+    [SerializeField] Weapon pistol;
     // Start is called before the first frame update
     void Start()
     {
         mm = FindObjectOfType<MoneyManager>();
+
         ic = FindObjectOfType<UIInterfaceController>();
 
         uiObject.SetActive(false);
@@ -25,7 +31,10 @@ public class PowerUpShop : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        if (powerNotif.enabled && (Time.time >= 20))
+        {
+            powerNotif.gameObject.SetActive(false);
+        }
     }
 
     private void OnTriggerStay(Collider other)
@@ -42,7 +51,24 @@ public class PowerUpShop : MonoBehaviour
             {
                 mm.RemoveMoney(cost);
                 uiObject.SetActive(false);
-                MoreHealthPower();
+                int select = Random.Range(1, 3);
+
+                switch(select)
+                {
+                    case 1:
+                        MoreHealthPower();
+                        powerNotif.gameObject.SetActive(true);
+                        powerNotif.text = "Power Up acquired: More Health!";
+                        break;
+                    case 2:
+                        DamageUpPower();
+                        powerNotif.gameObject.SetActive(true);
+                        powerNotif.text = "Power Up acquired: More Damage!";
+                        break;
+                    default:
+                        MoreHealthPower();
+                        break;
+                }
                 Debug.Log("Purchased");
             }
         }
@@ -59,18 +85,10 @@ public class PowerUpShop : MonoBehaviour
         ic.setMaxHealth(ic.getMaxHealth() + 50);
     }
 
-    private void NoReloadPower()
-    {
-
-    }
-
     private void DamageUpPower()
     {
-
+        m4.damage = m4.damage + 5;
+        pistol.damage = pistol.damage + 5;
     }
 
-    private void ExplosiveShotPower()
-    {
-
-    }
 }
